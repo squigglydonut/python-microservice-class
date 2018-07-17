02edb8a6edae8c9f525cbd130f2f26442c6a22c6
Zachary Moore <squigglydonut>
2018-07-16 21:05:03 -0700

    next

--- app/app.py
+++ app/app.py
@@ -7,7 +7,6 @@ def get_temperature():
   resp = requests.get(
       'https://api.darksky.net/forecast/cc451c6cb0b74ab2606660e8ffc3ec29/37.8267,-122.4233')
   temperature = resp.json()['currently']['temperature']
-  print(temperature)
   return temperature
 
 @app.route('/')
