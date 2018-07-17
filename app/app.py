b9df2d0c66c9d1e6d802721e2b1d21b3bce3a98e
Zachary Moore <squigglydonut>
2018-07-16 20:52:11 -0700

    jinja templates

---
+++ app/app.py
@@ -0,0 +1,19 @@
+import requests
+from flask import Flask, render_template
+
+def get_temperature():
+  resp = requests.get(
+      'https://api.darksky.net/forecast/cc451c6cb0b74ab2606660e8ffc3ec29/37.8267,-122.4233')
+  temperature = resp.json()['currently']['temperature']
+  print(temperature)
+  return temperature
+
+@app.route('/')
+def index():
+  temp = get_temperature()
+  return render_template('index.html', temp = temp)
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+  app.run()
